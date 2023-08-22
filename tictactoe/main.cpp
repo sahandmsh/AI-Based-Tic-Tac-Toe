769e7f35f5904c69e4d1388aea3e788ce560fe02
Sahand Mosharafian <82970651+sahandmsh@users.noreply.github.com>
2023-08-22 15:05:32 -0700

    Update main.cpp

--- tictactoe/main.cpp
+++ tictactoe/main.cpp
@@ -5,7 +5,7 @@ int main(){
     srand( int ( time(0) ) ); 
     TTT G;
     std::cout<< "AI-Based bot is learning! Please wait :)\n";
-    G.episodic_learning(2);
+    G.episodic_learning(2000000);
     std::cout<< "\n\n\n Now it's time to play against the AI-trained bot\n\n\n";
     while (true){
         std::cout<<"Want to stop playing? enter \"NO\" now! Otherwise, press enter!\n";
